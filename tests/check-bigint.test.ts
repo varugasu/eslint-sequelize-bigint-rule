2b4127b895d11a0aa0e09939dea88a787b9fa366
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-08-09 23:13:51 -0300

    test: add rule tester

--- tests/check-bigint.test.ts
+++ tests/check-bigint.test.ts
@@ -1,9 +1,45 @@
+import * as mocha from "mocha";
 import { RuleTester } from "@typescript-eslint/rule-tester";
 import rule from "../eslint/check-bigint";
+
+RuleTester.afterAll = mocha.after;
 
 const ruleTest = new RuleTester();
 
 ruleTest.run("check-bigint", rule, {
-  valid: [`class A {}`],
-  invalid: [],
+  valid: [
+    `
+  import { Column, Model as SequelizeModel, DataType } from "sequelize-typescript";
+
+  export class User extends SequelizeModel {
+    @Column({ type: DataType.BIGINT })
+    aBigIntColumn?: bigint;
+  }`,
+    `
+  import { Column, Model } from "sequelize-typescript";
+
+  export class User extends Model {
+    @Column
+    aBigIntColumn?: int;
+  }`,
+  ],
+  invalid: [
+    {
+      code: `
+      import { Column, Model as SequelizeModel, DataType } from "sequelize-typescript";
+    
+      export class User extends SequelizeModel {
+        @Column({ type: DataType.BIGINT })
+        aBigIntColumn?: number;
+      }`,
+      errors: [{ messageId: "useBigInt" }],
+      output: `
+      import { Column, Model as SequelizeModel, DataType } from "sequelize-typescript";
+    
+      export class User extends SequelizeModel {
+        @Column({ type: DataType.BIGINT })
+        aBigIntColumn?: bigint;
+      }`,
+    },
+  ],
 });
